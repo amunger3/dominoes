6ef6e7e45333f0348b235b4192e3b42ada3555e9
amunger3 <munger.alex@gmail.com>
2020-11-01 17:32:09 -0600

    changed info

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@ from setuptools import setup
 
 setup(
     name='dominoes',
-    version='6.1.0',
+    version='1.0.0',
     description='A Python library for the game of dominoes, with an accompanying CLI and AI players.',
-    url='https://github.com/abw333/dominoes',
+    url='https://github.com/amunger3/dominoes',
     author='Alan Wagner',
-    author_email='alanwagner333@gmail.com',
+    author_email='munger.alex@gmail.com',
     license='MIT',
     packages=['dominoes'],
     scripts=['bin/dominoes'],
